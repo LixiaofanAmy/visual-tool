ce47547d4e9b42e0620358f5ac5dd235818f5bac
lixiaofan_amy <>
2021-02-04 23:32:55 +0800

    添加图层工具。

---
+++ src/views/edit/components/LayerTool.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="z-index">
+    <span
+      v-for="component in components"
+      :key="component.zIndex"
+      @click="changeLayer(component)"
+    >{{component.typeName}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayerTool',
+  computed: {
+    components() {
+      return this.$store.state.components
+    }
+  },
+  methods: {
+    changeLayer(component) {
+      const index = this.components.indexOf(component)
+      this.$store.commit('changeBoxActivated', index)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
