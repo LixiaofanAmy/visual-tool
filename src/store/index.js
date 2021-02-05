63338a767799c912d176553397719f15c24e1b7e
lihe <1536598246@qq.com>
2021-02-05 18:40:25 +0800

    添加注释。

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 数据可视化云图名称
     visualName: '测试',
+    // 活动组件数组
     components: [],
+    // 活动组件索引
+    currentIndex: -1,
+    // 快照数组
     timeline: [[]],
+    // 快照索引
     current: 0,
-    limit: 1000,
-    currentIndex: -1
+    // 撤销、重做保存快照最大数量
+    limit: 1000
   },
   mutations: {
     // 撤销
